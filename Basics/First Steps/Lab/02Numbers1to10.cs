0f2c180f1759ba509824d80a0bb4bbcfab56604b
Petar Todorov <154418284+PetarTdrv@users.noreply.github.com>
2024-10-27 20:41:35 +0200

    Create 02Numbers1to10.cs

---
+++ Basics/First Steps/Lab/02Numbers1to10.cs
@@ -0,0 +1,12 @@
+Console.WriteLine(1);
+Console.WriteLine(2);
+Console.WriteLine(3);
+Console.WriteLine(4);
+Console.WriteLine(5);
+Console.WriteLine(6);
+Console.WriteLine(7);
+Console.WriteLine(8);
+Console.WriteLine(9);
+Console.WriteLine(10);
+
+//Here we can make a for loop but this is first exercise in the lessons:)
